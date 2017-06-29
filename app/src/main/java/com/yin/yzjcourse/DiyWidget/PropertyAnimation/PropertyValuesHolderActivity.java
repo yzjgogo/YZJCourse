916f7f30b480527f101433b6d84101e242ee0924
殷志杰 <yzjgogo@139.com>
2017-06-29 15:28:54 +0800

    Keyframe

--- app/src/main/java/com/yin/yzjcourse/DiyWidget/PropertyAnimation/PropertyValuesHolderActivity.java
+++ app/src/main/java/com/yin/yzjcourse/DiyWidget/PropertyAnimation/PropertyValuesHolderActivity.java
@@ -1,5 +1,7 @@
 package com.yin.yzjcourse.DiyWidget.PropertyAnimation;
 
+import android.animation.Animator;
+import android.animation.Keyframe;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.animation.TypeEvaluator;
@@ -7,6 +9,8 @@ import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.LinearInterpolator;
 import android.widget.LinearLayout;
 
 import com.yin.yzjcourse.DiyWidget.PropertyAnimation.View.MyTextView;
@@ -28,7 +32,10 @@ public class PropertyValuesHolderActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick({R.id.bt_float_int, R.id.bt_object, R.id.bt_valueanimator})
+    @OnClick({R.id.bt_float_int, R.id.bt_object, R.id.bt_valueanimator,
+            R.id.bt_keyframe_of_float,R.id.bt_keyframe_of_int,R.id.bt_keyframe_of_object,
+            R.id.bt_keyframe_use,R.id.bt_keyframe_phone
+    })
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.bt_float_int:
@@ -39,6 +46,20 @@ public class PropertyValuesHolderActivity extends AppCompatActivity {
                 break;
             case R.id.bt_valueanimator:
                 showValueAnimatorHolder();
+                break;
+            case R.id.bt_keyframe_of_float:
+                showKeyframeOfFloat();
+                break;
+            case R.id.bt_keyframe_of_int:
+                showKeyframeOfInt();
+            case R.id.bt_keyframe_of_object:
+                showKeyframeOfObject();
+                break;
+            case R.id.bt_keyframe_use:
+                showKeyframeUse();
+                break;
+            case R.id.bt_keyframe_phone:
+                showKeyframePhone();
                 break;
         }
     }
@@ -104,6 +125,135 @@ public class PropertyValuesHolderActivity extends AppCompatActivity {
             int curInt = (int) (startInt + fraction * (endInt - startInt));
             char result = (char) curInt;
             return result;
+        }
+    }
+    private void showKeyframeOfFloat() {
+        Keyframe keyframe0 = Keyframe.ofFloat(0f);
+        keyframe0.setValue(0);
+        Keyframe keyframe1 = Keyframe.ofFloat(0.1f);
+        keyframe1.setValue(-20f);
+        Keyframe keyframe2 = Keyframe.ofFloat(0.2f,20f);
+        Keyframe keyframe3 = Keyframe.ofFloat(0.3f,-20f);
+        Keyframe keyframe4 = Keyframe.ofFloat(0.4f,20f);
+        Keyframe keyframe5 = Keyframe.ofFloat(0.5f,-20f);
+        Keyframe keyframe6 = Keyframe.ofFloat(0.6f,20f);
+        Keyframe keyframe7 = Keyframe.ofFloat(0.7f,-20f);
+        Keyframe keyframe8 = Keyframe.ofFloat(0.8f,20f);
+        Keyframe keyframe9 = Keyframe.ofFloat(0.9f,-20f);
+        Keyframe keyframe10 = Keyframe.ofFloat(1f,0);
+        PropertyValuesHolder valuesHolder = PropertyValuesHolder.ofKeyframe("rotation",keyframe0,keyframe1,keyframe2,keyframe3,keyframe4,keyframe5,keyframe6,keyframe7,keyframe8,keyframe9,keyframe10);
+        ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(tvTarget,valuesHolder);
+        animator.setDuration(3000);
+        animator.start();
+    }
+
+    private void showKeyframeOfInt() {
+        Keyframe keyframe0 = Keyframe.ofInt(0f,0x00000000);
+        Keyframe keyframe1 = Keyframe.ofInt(1f,0xff000000);
+        PropertyValuesHolder valuesHolder = PropertyValuesHolder.ofKeyframe("BackgroundColor",keyframe0,keyframe1);
+        ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(tvTarget,valuesHolder);
+        animator.setDuration(3000);
+        animator.start();
+    }
+
+    private void showKeyframeOfObject() {
+        Keyframe keyframe0 = Keyframe.ofObject(0f,new Character('A'));
+        Keyframe keyframe1 = Keyframe.ofObject(0.1f,new Character('L'));
+        Keyframe keyframe2 = Keyframe.ofObject(1f,new Character('Z'));
+        PropertyValuesHolder valuesHolder = PropertyValuesHolder.ofKeyframe("CharText",keyframe0,keyframe1,keyframe2);
+        //使用ofObject要给PropertyValuesHolder设置自定义Evaluator
+        valuesHolder.setEvaluator(new CharEvaluator());
+        ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(tvTarget, valuesHolder);
+        animator.setDuration(3000);
+        animator.start();
+    }
+
+
+    /**
+     给某一个KeyFrame设置插值器，该插值器会作用于上一个Keyframe到当前Keyframe;
+     例如给Keyframe4设置插值器，则该插值器的作用范围是从Keyframe3到Keyframe4
+     *
+     如果去掉第0帧，将以第一个关键帧为起始位置
+     如果去掉结束帧，将以最后一个关键帧为结束位置
+     使用Keyframe来构建动画，至少要有两个或两个以上帧
+     */
+    private void showKeyframeUse() {
+        Keyframe frame0 = Keyframe.ofFloat(0f, 0);
+        Keyframe frame1 = Keyframe.ofFloat(0.5f, 100f);
+        frame1.setInterpolator(new LinearInterpolator());
+        Keyframe frame2 = Keyframe.ofFloat(1,0);
+        PropertyValuesHolder frameHolder = PropertyValuesHolder.ofKeyframe("rotation", frame0,frame1,frame2);
+
+        Animator animator = ObjectAnimator.ofPropertyValuesHolder(tvTarget, frameHolder);
+        animator.setDuration(3000);
+        animator.start();
+    }
+
+    private void showKeyframePhone() {
+        /**
+         * 左右震动效果
+         */
+        Keyframe frame0 = Keyframe.ofFloat(0f, 0);
+        Keyframe frame1 = Keyframe.ofFloat(0.1f, -20f);
+        Keyframe frame2 = Keyframe.ofFloat(0.2f, 20f);
+        Keyframe frame3 = Keyframe.ofFloat(0.3f, -20f);
+        Keyframe frame4 = Keyframe.ofFloat(0.4f, 20f);
+        Keyframe frame5 = Keyframe.ofFloat(0.5f, -20f);
+        Keyframe frame6 = Keyframe.ofFloat(0.6f, 20f);
+        Keyframe frame7 = Keyframe.ofFloat(0.7f, -20f);
+        Keyframe frame8 = Keyframe.ofFloat(0.8f, 20f);
+        Keyframe frame9 = Keyframe.ofFloat(0.9f, -20f);
+        Keyframe frame10 = Keyframe.ofFloat(1, 0);
+        PropertyValuesHolder frameHolder1 = PropertyValuesHolder.ofKeyframe("rotation", frame0, frame1, frame2, frame3, frame4, frame5, frame6, frame7, frame8, frame9, frame10);
+
+
+/**
+ * scaleX放大1.1倍
+ */
+        Keyframe scaleXframe0 = Keyframe.ofFloat(0f, 1);
+        Keyframe scaleXframe1 = Keyframe.ofFloat(0.1f, 1.1f);
+        Keyframe scaleXframe2 = Keyframe.ofFloat(0.2f, 1.1f);
+        Keyframe scaleXframe3 = Keyframe.ofFloat(0.3f, 1.1f);
+        Keyframe scaleXframe4 = Keyframe.ofFloat(0.4f, 1.1f);
+        Keyframe scaleXframe5 = Keyframe.ofFloat(0.5f, 1.1f);
+        Keyframe scaleXframe6 = Keyframe.ofFloat(0.6f, 1.1f);
+        Keyframe scaleXframe7 = Keyframe.ofFloat(0.7f, 1.1f);
+        Keyframe scaleXframe8 = Keyframe.ofFloat(0.8f, 1.1f);
+        Keyframe scaleXframe9 = Keyframe.ofFloat(0.9f, 1.1f);
+        Keyframe scaleXframe10 = Keyframe.ofFloat(1, 1);
+        PropertyValuesHolder frameHolder2 = PropertyValuesHolder.ofKeyframe("ScaleX", scaleXframe0, scaleXframe1, scaleXframe2, scaleXframe3, scaleXframe4, scaleXframe5, scaleXframe6, scaleXframe7, scaleXframe8, scaleXframe9, scaleXframe10);
+
+
+/**
+ * scaleY放大1.1倍
+ */
+        Keyframe scaleYframe0 = Keyframe.ofFloat(0f, 1);
+        Keyframe scaleYframe1 = Keyframe.ofFloat(0.1f, 1.1f);
+        Keyframe scaleYframe2 = Keyframe.ofFloat(0.2f, 1.1f);
+        Keyframe scaleYframe3 = Keyframe.ofFloat(0.3f, 1.1f);
+        Keyframe scaleYframe4 = Keyframe.ofFloat(0.4f, 1.1f);
+        Keyframe scaleYframe5 = Keyframe.ofFloat(0.5f, 1.1f);
+        Keyframe scaleYframe6 = Keyframe.ofFloat(0.6f, 1.1f);
+        Keyframe scaleYframe7 = Keyframe.ofFloat(0.7f, 1.1f);
+        Keyframe scaleYframe8 = Keyframe.ofFloat(0.8f, 1.1f);
+        Keyframe scaleYframe9 = Keyframe.ofFloat(0.9f, 1.1f);
+        Keyframe scaleYframe10 = Keyframe.ofFloat(1, 1);
+        PropertyValuesHolder frameHolder3 = PropertyValuesHolder.ofKeyframe("ScaleY", scaleYframe0, scaleYframe1, scaleYframe2, scaleYframe3, scaleYframe4, scaleYframe5, scaleYframe6, scaleYframe7, scaleYframe8, scaleYframe9, scaleYframe10);
+
+/**
+ * 构建动画
+ */
+        Animator animator = ObjectAnimator.ofPropertyValuesHolder(tvTarget, frameHolder1, frameHolder2, frameHolder3);
+        animator.setDuration(1000);
+        animator.start();
+    }
+    class CharEvaluator implements TypeEvaluator<Character>{
+
+        @Override
+        public Character evaluate(float fraction, Character startValue, Character endValue) {
+            int start = (int)startValue;
+            int end = (int)endValue;
+            return (char)(start+(end-start)*fraction);
         }
     }
 }
