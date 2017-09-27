fa505748efe111a075ef4ee85e3db8743607b571
yzjgogo <yzjdev@139.com>
2017-09-27 10:54:52 +0800

    View的偏移

--- app/src/main/java/com/yin/yzjcourse/Coordinator/CustomTextBehaviorActivity.java
+++ app/src/main/java/com/yin/yzjcourse/Coordinator/CustomTextBehaviorActivity.java
@@ -11,44 +11,53 @@ import com.yin.yzjcourse.BaseActivity;
 import com.yin.yzjcourse.R;
 
 public class CustomTextBehaviorActivity extends BaseActivity {
-    private Button btGetY, btSetY, btGet;
-    private TextView tvShowY, tvFab, tvShow;
-    private EditText etInputY;
+    private Button btGetTransY, btSetTransY, btGetTop, btGetY;
+    private TextView tvShowTransY, tvFab, tvShowTop,tvShowY;
+    private EditText etInputTransY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_text_behavior);
-        btGetY = (Button) findViewById(R.id.bt_get_y);
-        btGet = (Button) findViewById(R.id.bt_get);
-        btSetY = (Button) findViewById(R.id.bt_set_y);
-        tvShowY = (TextView) findViewById(R.id.tv_show_y);
-        tvShow = (TextView) findViewById(R.id.tv_show);
+        btGetTransY = (Button) findViewById(R.id.bt_get_trans_y);
+        btGetTop = (Button) findViewById(R.id.bt_get_top);
+        btSetTransY = (Button) findViewById(R.id.bt_set_trans_y);
+        tvShowTransY = (TextView) findViewById(R.id.tv_show_trans_y);
+        tvShowTop = (TextView) findViewById(R.id.tv_show_top);
         tvFab = (TextView) findViewById(R.id.fab);
-        etInputY = (EditText) findViewById(R.id.et_input_y);
+        etInputTransY = (EditText) findViewById(R.id.et_input_trans_y);
+        btGetY = (Button) findViewById(R.id.bt_get_y);
+        tvShowY = (TextView) findViewById(R.id.tv_show_y);
         //设置垂直偏移：负数向上偏移，正数向下偏移
-        btSetY.setOnClickListener(new View.OnClickListener() {
+        btSetTransY.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //每次调用该方法都是在view原始top值的基础上进行垂直偏移，而不是在上次偏移后的top值的基础上偏移，且如果和上次的偏移值一样也不会偏移，因为不变
-                //例如：view的原始top值是500，第一次调用setTranslationY(100)则top在500的基础上变为600，第二次调用setTranslationY(200)则top也是在500的基础上变为700
+                //每次调用该方法都是在view的top值(无论怎么偏移都不变，getY()会随着偏移改变)的基础上进行垂直偏移，而不是在上次偏移后的getY()值的基础上偏移，且如果和上次的偏移值一样也不会偏移，因为不变
+                //例如：view的top值是500,getY()也是500，第一次调用setTranslationY(100)则top不变，getY()在500的基础上变为600，第二次调用setTranslationY(200)则top还不变，getY()在500的基础上变为700
                 //而不是在600的基础上变为800。
                 //如果第一次调用setTranslationY(100)第二次也调用setTranslationY(100)，则第二次不执行，因为偏移距离相同，进去看该方法
-                tvFab.setTranslationY(Float.parseFloat(etInputY.getText().toString().trim()));
+                tvFab.setTranslationY(Float.parseFloat(etInputTransY.getText().toString().trim()));
             }
         });
         //获取垂直偏移
-        btGetY.setOnClickListener(new View.OnClickListener() {
+        btGetTransY.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tvShowY.setText(tvFab.getTranslationY() + "");
+                tvShowTransY.setText(tvFab.getTranslationY() + "");
             }
         });
-        //获取top坐标
-        btGet.setOnClickListener(new View.OnClickListener() {
+        //获取top坐标,getTop()的值始终是不变的，当没有发生偏移时getTop()=getY();
+        btGetTop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tvShow.setText(tvFab.getY() + "");
+                tvShowTop.setText(tvFab.getTop() + "");
+            }
+        });
+        //getY() = getTop()+getTranslationY();
+        btGetY.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                tvShowY.setText(tvFab.getY()+"");
             }
         });
         //通过setTranslationY等方法使view发生偏移则是真正的view的移动，无论怎么偏移都会相应单击事件
