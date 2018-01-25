80fcbd84b44262fd4bbdf0928b67bc27ce2ca46a
yzjgogo <yzjdev@139.com>
2018-01-25 16:51:11 +0800

    是打发第三方

--- app/src/main/java/com/yin/yzjcourse/DiyWidget/ViewDispatchTouchActivity.java
+++ app/src/main/java/com/yin/yzjcourse/DiyWidget/ViewDispatchTouchActivity.java
@@ -24,9 +24,6 @@ public class ViewDispatchTouchActivity extends BaseActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
         setContentView(R.layout.activity_view_dispatch_touch);
         ButterKnife.bind(this);
     }
