76b02396064138d20d3a669fb37025ab4fb08845
yzjgogo <yzjdev@139.com>
2018-03-22 18:02:05 +0800

    修改skewView

--- app/src/main/java/com/yin/yzjcourse/DiyWidget/DrawView/DrawViewActivity.java
+++ app/src/main/java/com/yin/yzjcourse/DiyWidget/DrawView/DrawViewActivity.java
@@ -1,8 +1,10 @@
 package com.yin.yzjcourse.DiyWidget.DrawView;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.animation.BounceInterpolator;
 import android.widget.Button;
 
 import com.yin.yzjcourse.BaseActivity;
@@ -16,12 +18,20 @@ public class DrawViewActivity extends BaseActivity {
 
     @BindView(R.id.tv_bse)
     Button tvBse;
+    @BindView(R.id.sv_Skew)
+    SkewView svSkew;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw_view);
         ButterKnife.bind(this);
+
+        ObjectAnimator animator = ObjectAnimator.ofInt(svSkew,"angle",0,90);
+        animator.setDuration(9000);
+        animator.setRepeatMode(ValueAnimator.RESTART);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.start();
     }
 
     @OnClick(R.id.tv_bse)
--- app/src/main/java/com/yin/yzjcourse/DiyWidget/DrawView/SkewView.java
+++ app/src/main/java/com/yin/yzjcourse/DiyWidget/DrawView/SkewView.java
@@ -19,6 +19,7 @@ import com.yin.yzjcourse.utils.DLog;
 public class SkewView extends View {
     private Paint paintGreen;
     private Paint paintRed;
+    private int angle = 0;
     public SkewView(Context context) {
         super(context);
         initView();
@@ -59,7 +60,13 @@ public class SkewView extends View {
 //        DLog.eLog("结果："+Math.tan(Math.toRadians(60)));
         //skew参数是倾斜角度的tan值，并不是倾斜角度
         //skew的结果类似于纸片在空中飞舞时，人眼在某一刻的角度看到的情况
-        canvas.skew((float) Math.tan(Math.toRadians(60)),(float) Math.tan(Math.toRadians(60)));//X轴倾斜60度，Y轴不变
+//        float sx:将画布在x方向上倾斜相应的角度，sx倾斜角度的tan值，
+//        float sy:将画布在y轴方向上倾斜相应的角度，sy为倾斜角度的tan值，
+        canvas.skew((float) Math.tan(Math.toRadians(angle)),(float) Math.tan(Math.toRadians(0)));//X轴倾斜60度，Y轴不变
         canvas.drawRect(rect1, paintRed);
+    }
+    public void setAngle(int angle){
+        this.angle = angle;
+        invalidate();
     }
 }
