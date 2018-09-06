b7edb77aebd1e13478ec25d6e10a6f5dfb9a43e6
yzjgogo <yzjdev@139.com>
2018-09-06 14:30:46 +0800

    vp动画

--- app/src/main/java/com/yin/yzjcourse/Base/VPTransformerActivity.java
+++ app/src/main/java/com/yin/yzjcourse/Base/VPTransformerActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import com.yin.yzjcourse.Base.adapter.VPTransAdapter;
 import com.yin.yzjcourse.BaseActivity;
 import com.yin.yzjcourse.R;
-import com.yin.yzjcourse.utils.DLog;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,8 +16,8 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class VPTransformerActivity extends BaseActivity {
-    private static final float MAX_ALPHA=0.5f;
-    private static final float MAX_SCALE=0.9f;
+    private static final float MAX_ALPHA = 0.5f;
+    private static final float MAX_SCALE = 0.9f;
     @BindView(R.id.vp)
     ViewPager vp;
     private VPTransAdapter transAdapter;
@@ -64,30 +63,40 @@ public class VPTransformerActivity extends BaseActivity {
         views.add(view5);
         views.add(view6);
         views.add(view7);
-        transAdapter = new VPTransAdapter(views,this);
+        transAdapter = new VPTransAdapter(views, this);
         vp.setOffscreenPageLimit(3);
 //        vp.setPageMargin(30);
         vp.setAdapter(transAdapter);
         vp.setPageTransformer(true, new ViewPager.PageTransformer() {
+            /**
+             * position解释：
+             * 前提，一共加载多少页由setOffscreenPageLimit方法的传值决定；
+             * 以当前有5页被加载为例
+             * 静止时，这5页的position分别是：-2.0、-1.0、0.0(当前页)、1.0、2.0;
+             * 滑动时，因为position是float类型的，以当前页向左滑动为例，当前页的position由0.0 -> -1.0
+             *          所有的页面都这样过度
+             * @param page
+             * @param position
+             */
             @Override
             public void transformPage(View page, float position) {
-//                DLog.eLog("此时的位置：" +page.getTag().toString()+","+ position);
-                if(position<-1||position>1){
+//                DLog.eLog("此时的位置：" +page.getTag().toString()+"#"+ position);
+                if (position < -1 || position > 1) {
                     //不可见区域
                     page.setAlpha(MAX_ALPHA);
                     page.setScaleX(MAX_SCALE);
                     page.setScaleY(MAX_SCALE);
-                }else {
+                } else {
                     //可见区域，透明度效果
-                    if(position<=0){
+                    if (position <= 0) {
                         //pos区域[-1,0)
-                        page.setAlpha(MAX_ALPHA+MAX_ALPHA*(1+position));
-                    }else{
+                        page.setAlpha(MAX_ALPHA + MAX_ALPHA * (1 + position));
+                    } else {
                         //pos区域[0,1]
-                        page.setAlpha(MAX_ALPHA+MAX_ALPHA*(1-position));
+                        page.setAlpha(MAX_ALPHA + MAX_ALPHA * (1 - position));
                     }
                     //可见区域，缩放效果
-                    float scale=Math.max(MAX_SCALE,1-Math.abs(position));
+                    float scale = Math.max(MAX_SCALE, 1 - Math.abs(position));
                     page.setScaleX(scale);
                     page.setScaleY(scale);
                 }
