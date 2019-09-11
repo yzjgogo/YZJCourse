4266d6bba5cb8162396cd2debf47292d08bd2ab9
yzjgogo <yzjgogo@139.com>
2019-09-11 14:18:29 +0800

    View的getHitRect()方法

--- app/src/main/java/com/yin/yzjcourse/DiyWidget/ViewLocalRectActivity.java
+++ app/src/main/java/com/yin/yzjcourse/DiyWidget/ViewLocalRectActivity.java
@@ -7,6 +7,7 @@ import android.widget.RelativeLayout;
 
 import com.yin.yzjcourse.BaseActivity;
 import com.yin.yzjcourse.R;
+import com.yin.yzjcourse.Utils;
 import com.yin.yzjcourse.utils.DLog;
 
 import java.util.Arrays;
@@ -15,6 +16,9 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+/**
+ * https://www.jianshu.com/p/f131a390eb52
+ */
 public class ViewLocalRectActivity extends BaseActivity {
 
     @BindView(R.id.test_view)
@@ -33,25 +37,30 @@ public class ViewLocalRectActivity extends BaseActivity {
     public void onViewClicked() {
         int[] location = new int[2];
         container.getLocationOnScreen(location);//目的是为了知道状态栏的高度
-        DLog.eLog("容器："+ Arrays.toString(location));
+        DLog.eLog("容器："+ Arrays.toString(location)+",screenWidth:"+ Utils.getScreenWidth(this)+",screenHeight:"+(Utils.getScreenHeight(this)+location[1]));
 
         /**
-         * view.getLocalVisibleRect(localRect)
-         * 返回view的可视区域相对自身的坐标系的矩形区域localRect,注意坐标系是相对view自身的，因此view的真实左上角为坐标系的(0,0);
-         * 所谓真实左上角就是view的实际左上角，而不是view可视区域的左上角；
-         * 可视区域与invisible无关，可以理解成有效区域，并不是说invisible返回的矩形就没有了，例如layout_marginLeft="-100px",那么这溢出去的100px就不是可视区域。剩下的才是可视区域
+         * view.getLocalVisibleRect(localRect),返回true，说明view没有完全离开可滚动区域；返回false说明view离开可滚动区域了。
+         * 一般是针对view的祖辈viewGroup中有可滚动的viewGroup的情况，或者view的某个方向的margin有负值的情况，例如marginLeft=-100px。
+         * 如果view没有完全离开可滚动区域，则返回view在可滚动区域内的部分相对自身的坐标系的矩形区域localRect,注意坐标系是相对view自身的，因此view的真实左上角为坐标系的(0,0)，返回值为true。
+         * 所谓真实左上角就是view的实际左上角，而不是view在可滚动区域内部的左上角；
+         * 所谓可滚动区域就是可滚动控件的可见区域,例如ScrollView的可见区域。
+         * 如果view完全离开可滚动区域，则localRect是以可滚动的viewGroup的左上角为坐标系的矩形区域(view所占的整个区域)，而不再是以view自身的左上角为坐标系了。此时返回false。
          */
         Rect localRect = new Rect();
-        testView.getLocalVisibleRect(localRect);
-        DLog.eLog("local："+localRect.toString());
+        boolean localBool = testView.getLocalVisibleRect(localRect);
+        DLog.eLog("local："+localRect.toString()+","+localBool);
 
         /**
-         * view.getGlobalVisibleRect(rect)
-         * 返回view的可视区域在屏幕中的矩形区域，注意是相对屏幕的，因此rect所在的坐标系是屏幕，原点就是屏幕的左上角(0,0)
-         * 可视区域与invisible无关，可以理解成有效区域，并不是说invisible返回的矩形就没有了，例如layout_marginLeft="-100px",那么这溢出去的100px就不是可视区域。剩下的才是可视区域
+         * view.getGlobalVisibleRect(rect)，返回true，说明view没有完全离开可滚动区域；返回false说明view离开可滚动区域了。
+         * 一般是针对view的祖辈viewGroup中有可滚动的viewGroup的情况，或者view的某个方向的margin有负值的情况，例如marginLeft=-100px。
+         * 如果view没有完全离开可滚动区域，则返回view在可滚动区域内的部分相对屏幕的坐标系的矩形区域rect,注意坐标系是相对屏幕的，因此屏幕的左上角为坐标系的(0,0)，返回值为true。
+         * 如果view完全离开可滚动区域，则rect是以可滚动的viewGroup的左上角为坐标系的矩形区域(view所占的整个区域)，而不再是以屏幕的左上角为坐标系了。此时返回false。
          */
         Rect globalRect = new Rect();
-        testView.getGlobalVisibleRect(globalRect);
-        DLog.eLog("global："+globalRect.toString());
+        boolean golobalBool = testView.getGlobalVisibleRect(globalRect);
+        int[] globalLocation = new int[2];
+        testView.getLocationOnScreen(globalLocation);
+        DLog.eLog("global："+globalRect.toString()+","+Arrays.toString(globalLocation)+","+golobalBool);
     }
 }
