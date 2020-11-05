438528d359307c64758ebe83da52b803cc08092a
yinzhijie <yzjgogo@139.com>
2020-11-05 13:55:24 +0800

    字符串分割类StringTokenizer

--- app/src/test/java/com/yin/yzjcourse/k5类和对象/k1_5属性和访问器/k2委托属性/k1引入1_属性变化监听原始代码/TestUsage.kt
+++ app/src/test/java/com/yin/yzjcourse/k5类和对象/k1_5属性和访问器/k2委托属性/k1引入1_属性变化监听原始代码/TestUsage.kt
@@ -1,6 +1,7 @@
 package com.yin.yzjcourse.k5类和对象.k1_5属性和访问器.k2委托属性.k1引入1_属性变化监听原始代码
 
 import org.junit.Test
+import java.beans.PropertyChangeEvent
 import java.beans.PropertyChangeListener
 
 class TestUsage {
@@ -9,6 +10,12 @@ class TestUsage {
         val p = Person("Jack", 34, 2000)
         p.addPropertyChangeListener(PropertyChangeListener { event ->
             println("属性${event.propertyName}从${event.oldValue}变到${event.newValue}")
+        })
+        //匿名内部类的方式
+        p.addPropertyChangeListener(object :PropertyChangeListener{
+            override fun propertyChange( event: PropertyChangeEvent){
+                println("属性${event.propertyName}从${event.oldValue}变到${event.newValue}")
+            }
         })
 
         //这里的it就是propertyChange(PropertyChangeEvent evt)里的evt对象
