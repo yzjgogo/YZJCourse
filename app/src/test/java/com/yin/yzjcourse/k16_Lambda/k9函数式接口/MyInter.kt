028d70b15a4a5547439178c693d32c052a4023c2
yinzhijie <yzjgogo@139.com>
2020-11-06 15:07:40 +0800

    Office-commit

--- app/src/test/java/com/yin/yzjcourse/k16_Lambda/k9函数式接口/MyInter.kt
+++ app/src/test/java/com/yin/yzjcourse/k16_Lambda/k9函数式接口/MyInter.kt
@@ -24,7 +24,7 @@ class MyInter {
         val a = 1;
         val myjava = MyJava()
         myjava.setRun { println("我在Runnable的run()中运行，我不会重复重建") }//这个lambda对应的匿名类可以复用，不会重复创建
-        myjava.setRun { println("我在Runnable的run()中运行，我会重复重建，$a") }//这个lambda对应的匿名类不可以复用，每次调用都会重复创建该匿名类，因为吃用其所在函数的变量
+        myjava.setRun { println("我在Runnable的run()中运行，我会重复重建，$a") }//这个lambda对应的匿名类不可以复用，每次调用都会重复创建该匿名类，因为持有其所在函数的变量
         val lam = { println("我不会重复创建")}
         myjava.setRun(lam)//lam不会重复创建，因为有变量接收
 
