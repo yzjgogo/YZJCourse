cc75c1f8b9ea85baade02deff42164cc6faa04ed
yinzhijie <yzjgogo@139.com>
2020-11-07 17:32:43 +0800

    office-commit

--- app/src/test/java/com/yin/yzjcourse/k21泛型/k4泛型运行时擦除/K1泛型擦除.kt
+++ app/src/test/java/com/yin/yzjcourse/k21泛型/k4泛型运行时擦除/K1泛型擦除.kt
@@ -40,8 +40,21 @@ class K1泛型擦除 {
         printSum(listOf("1","2"))//ClassCastException
     }
     fun printSum(c: Collection<*>) {
+        //因为<Person>被擦除，所以，强转成功得到结果pList，其实就是原String类型的list，只是泛型被忽略了
+        val pList = c as? List<Person>
+                ?: throw IllegalArgumentException("Person异常")
+        println("&&&&&&&&&&&&&& $pList &&&&&&")
+
+        //下面两行编译器认为类型不兼容Perosn
+//        println("&&&&&&&&&&&& ${pList[0] is Int} &&&")
+//        println("&&&&&&&&&&&& ${pList[0] is String} &&&")
+
+
+        //虽然你写了<Int>,但是运行时as并不知道他要强转成<Int>，因为<Int>被擦除了，所以得到的结果intList实际上还是String类型，在执行intList.sum()时会报错
         val intList = c as? List<Int>
                 ?: throw IllegalArgumentException("异常")
+        println(intList)
         println(intList.sum())
     }
+    class Person
 }
