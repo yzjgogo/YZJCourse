ab2036e16eb30e9452db68f94fc3bf16fdaeb6f3
yzjgogo <yzjdev@139.com>
2021-04-17 14:21:03 +0800

    home-提交

--- app/src/main/java/com/yin/yzjcourse/Jetpack/LiveDataActivity.kt
+++ app/src/main/java/com/yin/yzjcourse/Jetpack/LiveDataActivity.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.yin.yzjcourse.BaseActivity
 import com.yin.yzjcourse.R
+import com.yin.yzjcourse.utils.DLog
 
 class LiveDataActivity : BaseActivity() {
     lateinit var textView: TextView
@@ -23,7 +24,11 @@ class LiveDataActivity : BaseActivity() {
         val liveData = viewModel.currentSecond
 
         //给liveData添加一个观察者，当ViewModel里的数据发生变化时，被观察者LiveData会通知观察者
+        //只有activity处于前台(onStart,onResume)时，liveData数据发生变化时才会通知观察者，否则即使liveData的数据有变化，也不会通知观察者，
+        // 看observe(@NonNull LifecycleOwner owner, @NonNull Observer<? super T> observer) 的源码
+        //第一个参数是LifecycleOwner,在方法的最后，将observer于activity(LifecycleOwner)的生命周期关联起来，从而实现之。
         liveData?.observe(this){
+            DLog.eLog("收到来自liveData的数据变化：$it")
             textView.text = it.toString()
         }
 
