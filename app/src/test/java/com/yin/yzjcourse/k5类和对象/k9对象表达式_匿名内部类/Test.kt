77fbb8df0df77e1153862658c2bbfb8c2b538d93
yinzhijie <yzjgogo@139.com>
2021-08-27 10:09:16 +0800

    字符串分割类StringTokenizer

--- app/src/test/java/com/yin/yzjcourse/k5类和对象/k9对象表达式_匿名内部类/Test.kt
+++ app/src/test/java/com/yin/yzjcourse/k5类和对象/k9对象表达式_匿名内部类/Test.kt
@@ -12,7 +12,8 @@ class Test {
      * object关键字不仅能用来声明单例模式，还能用来声明匿名对象，但是匿名对象不是单例的；
      * 匿名对象代替了java中的匿名内部类的用法；
      * 匿名对象可以实现多个接口或不实现接口；
-     * 对象表达式中的代码可以访问创建它的函数里的变量，并且不需要用final修改，
+     * 对象表达式中的代码可以访问创建它的函数里的变量，并且不需要用final修改，之所以不需要用final修饰，是因为这个count被一个对象包裹起来了，实际上onFocus和onClick里对count的访问，是对那个包裹这个count的对象的访问
+     * Tools -> Kotlin -> Show Kotlin Bytecode -> Decompile查看
      */
     fun testinner(view: MyView){
         var count = 0//不需要用final修改
