c1fd24a72d39ba8eb946274001eaa9950172fa30
yinzhijie <yzjgogo@139.com>
2021-09-08 18:10:37 +0800

    字符串分割类StringTokenizer

--- app/src/test/java/com/yin/yzjcourse/k22协程/5异步流.kt
+++ app/src/test/java/com/yin/yzjcourse/k22协程/5异步流.kt
@@ -2,9 +2,7 @@ package com.yin.yzjcourse.k22协程
 
 //import kotlinx.coroutines.delay
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.FlowCollector
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 import org.junit.Test
 
 class `5异步流` {
@@ -29,7 +27,7 @@ class `5异步流` {
             }
             //流是冷的，当调用simple()时，flow(block)里的block并没有运行，这个block函数类型只是作为SafeFlow(block)的一个属性返回这个SafeFlow类型的示例，因此没有什么耗时操作，这也就是为什么
             //simple()没有定义成suspend函数；
-            //一旦调用了collect(flowCollector),才热起来，根据之前分析的调用流程，就会触发flowCollector.block()，此时block才执行，又因为block可能耗时，因为要把block定义成suspend函数，block()里调用了emit()，emit()也
+            //一旦调用了collect(flowCollector),才热起来，因此suspend设计成suspend函数，根据之前分析的调用流程，就会触发flowCollector.block()，此时block才执行，又因为block可能耗时，因为要把block定义成suspend函数，block()里调用了emit()，emit()也
             //可能耗时，所以emit()也定义成了suspend
             simple().collect(flowCollector)
 
@@ -63,14 +61,15 @@ class `5异步流` {
     一个个打印:2,main @coroutine#1
     Done
 
-
+    流的取消
      */
     @InternalCoroutinesApi
     @Test
     fun test2() {
+//        /*
         runBlocking<Unit> {
             //250ms后超时，超时自动执行协程的取消操作，delay()支持取消，取消后没有什么异常，没来得及emit()的将不会有机会发射而已，因为取消了
-            withTimeoutOrNull(250) { // 在 250 毫秒后超时
+            withTimeoutOrNull(7000) { // 在 250 毫秒后超时
                 simple2().collect(object : FlowCollector<Int> {
                     override suspend fun emit(value: Int) {
                         println("一个个打印:${value},${Thread.currentThread().name}")
@@ -79,13 +78,48 @@ class `5异步流` {
             }
             println("Done")
         }
+//        */
+
+
+/*
+这样测试取消也可以
+        runBlocking {
+            val job = launch {
+                simple2().collect(object : FlowCollector<Int> {
+                    override suspend fun emit(value: Int) {
+                        println("一个个打印:${value},${Thread.currentThread().name}")
+                    }
+                })
+            }
+            delay(7000)
+            job.cancelAndJoin()
+        }
+        */
     }
 
     fun simple2(): Flow<Int> = flow {
         for (i in 1..3) {
-            delay(100)
+            delay(3000)
             println("Emitting $i")
             emit(i)
+        }
+    }
+
+
+    @Test
+    fun test3(){
+        runBlocking {
+            flowOf(10010).collect {
+                println("打印出：$it")
+            }
+            /**
+                同理flowOf(10010)是冷的，并没有发生数据
+                直到collect()才热起来，因此collect()是suspend函数，它里面的action也是action函数
+
+                整个调用逻辑参考：flowof.png
+
+
+             */
         }
     }
 }
