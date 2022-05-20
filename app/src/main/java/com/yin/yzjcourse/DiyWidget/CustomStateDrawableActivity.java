e4b5fce764141e2b40baee3d10e727d3208ac83b
yinzhijie <yzjgogo@139.com>
2022-05-20 16:54:21 +0800

    北化股份和规范化

--- app/src/main/java/com/yin/yzjcourse/DiyWidget/CustomStateDrawableActivity.java
+++ app/src/main/java/com/yin/yzjcourse/DiyWidget/CustomStateDrawableActivity.java
@@ -10,6 +10,10 @@ import android.widget.TextView;
 
 import com.yin.yzjcourse.R;
 
+/**
+ * 参考：https://blog.csdn.net/lmj623565791/article/details/43752383?ops_request_misc=%257B%2522request%255Fid%2522%253A%2522165301184116782248564589%2522%252C%2522scm%2522%253A%252220140713.130102334.pc%255Fblog.%2522%257D&request_id=165301184116782248564589&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~blog~first_rank_ecpm_v1~rank_v31_ecpm-1-43752383-null-null.nonecase&utm_term=drawable&spm=1018.2226.3001.4450
+ * 鸿洋_的博客
+ */
 public class CustomStateDrawableActivity extends ListActivity {
 
     private Message[] messages = new Message[]{
