0dfcfb71d8f4b6c9dbb39c3b26209ecdd82d946b
yinzhijie <yzjgogo@139.com>
2022-06-10 15:10:40 +0800

    北化股份和规范化

--- app/src/main/java/com/yin/yzjcourse/Optimize/LargeImageActivity.java
+++ app/src/main/java/com/yin/yzjcourse/Optimize/LargeImageActivity.java
@@ -14,6 +14,7 @@ import java.io.InputStream;
  https://wenku.baidu.com/view/09685706de36a32d7375a417866fb84ae45cc3ae.html
  https://www.freesion.com/article/73441100030/
  https://blog.51cto.com/u_14202100/5189185
+ hongyang的CSDN博客：https://blog.csdn.net/lmj623565791/article/details/49300989?ops_request_misc=%257B%2522request%255Fid%2522%253A%2522165484306616782248554182%2522%252C%2522scm%2522%253A%252220140713.130102334.pc%255Fblog.%2522%257D&request_id=165484306616782248554182&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~blog~first_rank_ecpm_v1~rank_v31_ecpm-1-49300989-null-null.nonecase&utm_term=BitmapRegionDecoder&spm=1018.2226.3001.4450
  * */
 public class LargeImageActivity extends BaseActivity {
     private HighImageView photoView;
